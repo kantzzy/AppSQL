1ad7da5e9414b9b994ee55c6e517031bc3e2fe99
Kevin <kevinorigami2013@gmail.com>
2020-03-02 22:04:50 -0300

    ok

---
+++ app/src/main/java/com/example/appsqlite/DatabaseHelper.java
@@ -0,0 +1,67 @@
+package com.example.appsqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "DBLogin.sql", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tbUsuario(email text primary key, senha text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //db.execSQL("drop table if exists tbUsuario");
+    }
+    //inserindo valores no banco de dados
+
+    public boolean insert(String email, String senha) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email", email);
+        contentValues.put("senha", senha);
+
+        long inserido = db.insert("tbUsuario", null, contentValues);
+
+        if (inserido == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean validarEmail(String email) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("select * from tbUsuario where email = ?", new String[]{email});
+        if (cursor.getCount() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    //Verificando usuário e senha
+
+    public Boolean checarEmailSenha(String email, String senha) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from tbUsuario where email = ? and senha = ?", new String[]{email, senha});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/appsqlite/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.appsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+
+    EditText txtEmail, txtSenha, txtConfirmaSenha;
+    Button btnRegistar, btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);\
+
+        txtEmail = findViewById(R.id.idEmail);
+        txtSenha = findViewById(R.id.idSenha);
+        txtConfirmaSenha = findViewById(R.id.idConfirmaSenha);
+
+        btnLogin = findViewById(R.id.idBtnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegistar = findViewById(R.id.idBtnRegistrar);
+
+        btnRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, senha, confirmaSenha;
+
+                email = txtEmail.getText().toString();
+                senha = txtSenha.getText().toString();
+                confirmaSenha = txtConfirmaSenha.getText().toString();
+
+                if (email.equals("") || senha.equals("") || confirmaSenha.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Favor inserir valores!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (senha.equals(confirmaSenha)) {
+                        Boolean checharEmail = db.validarEmail(email);
+                        if (checharEmail == true) {
+                            Boolean inserir = db.insert(email, senha);
+                            if (inserir == true) {
+                                Toast.makeText(getApplicationContext(), "Registro inserido com sucesso!!!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Email inserido já existe!!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Senha não confere!!!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+    }
+
+}
